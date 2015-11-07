c06e560b69cdb4f3036c7ecc86a63f6e4fa396b2
Fahim Imaduddin Dalvi <dalvifahim@gmail.com>
2015-11-06 20:17:59 -0800

    Added function to create train, test split

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
+import numpy as np
+import util.analysis as analysis
 import util.io as io
 import util.preprocessing as preprocessing
 
 def main():
 	data = io.load_data()
-	print data['alive'][1].shape
-	print preprocessing.to_fixed_length(data['alive'], 57)[1].shape
+	X, y, class_names = preprocessing.create_data_tensor(data)	
+	X_train, y_train, X_test, y_test = preprocessing.create_train_test_split(X, y)
+
+	analysis.plot_signals_two_column(data[class_names[y[0]]][0][:, 0:3],
+									 X[0, 0:3, :].T,
+										['Raw X', 'Raw Y', 'Raw Z'], 
+										['Fixed X', 'Fixed Y', 'Fixed Z'])
 
 if __name__ == '__main__':
 	main()
--- src/util/preprocessing.py
+++ src/util/preprocessing.py
@@ -54,7 +54,7 @@ def create_data_tensor(data, series_length=57):
         num_samples += len(data[sign])
 
     X = np.zeros((num_samples, num_features, series_length))
-    y = np.zeros((num_samples))
+    y = np.zeros((num_samples), dtype=np.uint)
 
     sample_idx = 0
     for i, sign in enumerate(data.keys()):
@@ -66,3 +66,82 @@ def create_data_tensor(data, series_length=57):
 
     return X, y, class_names
 
+def create_train_test_split(X, y, test_size=0.3, shuffle=False, debug=False):
+    """
+    Splits data into a training and a testing set.
+
+    Args:
+        X: data tensor created by create_data_tensor
+        y: labels array created by create_data_tensor
+        test_size: size of test set. default: 30%
+        shuffle: boolean defining if the sets should be shuffled
+            before being split
+
+    Returns:
+        train_X:
+            data tensor containing training samples
+        train_y:
+            array containing class labels for training samples
+        test_X:
+            data tensor containing testing samples
+        test_y:
+            array containing class labels for testing samples
+    """
+    class_labels = np.unique(y)
+    train_samples_idx = []
+    test_samples_idx = []
+
+    # Shuffle the data
+    if shuffle:
+        if debug: print 'Shuffling data...',
+        shuffled_idx = np.arange(y.size)
+        np.random.shuffle(shuffled_idx)
+        X = X[shuffled_idx, :, :]
+        y = y[shuffled_idx]
+
+        if debug: print 'done'
+    
+    # Collect all the indices for the training and testing set
+    if debug: print 'Extracting sample indices...',
+    for label in class_labels:
+        idx = np.where(y==label)[0]
+        train_samples = int(len(idx) * (1-test_size))
+        test_samples = len(idx) - train_samples
+
+        train_samples_idx.append(idx[:train_samples])
+        test_samples_idx.append(idx[train_samples:])
+    if debug: print 'done'
+
+    # Concatenate all indices arrays into a single array of indices
+    if debug: print 'Collecting indices...',
+    train_samples_idx = np.concatenate(tuple(train_samples_idx))
+    test_samples_idx = np.concatenate(tuple(test_samples_idx))
+    if debug: print 'done'
+
+    if shuffle:
+        if debug: print 'Shuffling collecting indices...',
+        np.random.shuffle(train_samples_idx)
+        np.random.shuffle(test_samples_idx)
+        if debug: print 'done'
+
+    # Index and return the correct sets
+    if debug: print 'Returning sets...',
+    return X[train_samples_idx, :, :], y[train_samples_idx], X[test_samples_idx, :, :], y[test_samples_idx]
+        
+def test():
+    print 'Running tests for create_train_test_split'
+    X = np.random.rand(10,40,60)
+    y = np.array([0,1,2,3,1,4,4,2,3,4], dtype=np.uint)
+    tests = []
+    for i in xrange(0,5):
+        idx = np.where(y == i)[0]
+        test = np.sort(X[idx, :, :], axis=0)
+        tests.append(test)
+
+    X_train, y_train, X_test, y_test = create_train_test_split(X, y, test_size=0.5, shuffle=True)
+    for i in xrange(0,5):
+        test2 = np.sort(np.concatenate((X_train[np.where(y_train == i)[0], :, :], X_test[np.where(y_test == i)[0], :, :])), axis=0)
+        assert(np.sum(tests[i] == test2) == np.size(tests[i]))
+
+if __name__ == '__main__':
+    test()
