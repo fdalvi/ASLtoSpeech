9c45788751e0ac792d0d1c0012b7ecb0ed075b94
Hardie Cate <ccate@stanford.edu>
2015-11-09 17:15:53 -0800

    Baseline logistic regression implemented

--- src/baseline_logreg.py
+++ src/baseline_logreg.py
@@ -12,8 +12,8 @@ def run_logreg(quality='high'):
 	X_train, y_train, X_test, y_test = preprocessing.create_train_test_split(X, y, test_size=0.3, shuffle=True)
 
 	# flatten data
-	flattened_Xtrain = baseline_svm.flatten_matrix(X_train) 
-	flattened_Xtest = baseline_svm.flatten_matrix(X_test)
+	flattened_Xtrain = preprocessing.flatten_matrix(X_train) 
+	flattened_Xtest = preprocessing.flatten_matrix(X_test)
 
 	# fit logistic regression model
 	logreg_model = linear_model.LogisticRegression(multi_class='ovr')
